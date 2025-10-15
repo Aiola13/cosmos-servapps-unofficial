294d2ab4185669a025aea4e299fff9f75824323c
Aiola <7544268+Aiola13@users.noreply.github.com>
2025-10-15 16:18:52 +0200

    Add neko App & fix index.js icon path

--- index.js
+++ index.js
@@ -1,69 +1,81 @@
-const fs = require('fs')
-const { config } = require('process')
-const configFile = require('./config.json')
+const fs = require('fs');
+const { config } = require('process');
+const configFile = require('./config.json');
 
 // list all directories in the directory servapps and compile them in servapps.json
 
-const servapps = fs.readdirSync('./servapps').filter(file => fs.lstatSync(`./servapps/${file}`).isDirectory())
+const servapps = fs
+	.readdirSync('./servapps')
+	.filter(file => fs.lstatSync(`./servapps/${file}`).isDirectory());
 
-let servappsJSON = []
+let servappsJSON = [];
 
 for (const file of servapps) {
-  const servapp = require(`./servapps/${file}/description.json`)
-  servapp.id = file
-  servapp.screenshots = [];
-  servapp.artefacts = {};
+	const servapp = require(`./servapps/${file}/description.json`);
+	servapp.id = file;
+	servapp.screenshots = [];
+	servapp.artefacts = {};
 
-  // list all screenshots in the directory servapps/${file}/screenshots
-  const screenshots = fs.readdirSync(`./servapps/${file}/screenshots`)
-  for (const screenshot of screenshots) {
-    servapp.screenshots.push(`https://aiola13.github.io/cosmos-servapps-unofficial/servapps/${file}/screenshots/${screenshot}`)
-  }
+	// list all screenshots in the directory servapps/${file}/screenshots
+	const screenshots = fs.readdirSync(`./servapps/${file}/screenshots`);
+	for (const screenshot of screenshots) {
+		servapp.screenshots.push(
+			`https://aiola13.github.io/cosmos-servapps-unofficial/servapps/${file}/screenshots/${screenshot}`,
+		);
+	}
 
-  if(fs.existsSync(`./servapps/${file}/artefacts`)) {
-    const artefacts = fs.readdirSync(`./servapps/${file}/artefacts`)
-    for(const artefact of artefacts) {
-      servapp.artefacts[artefact] = (`https://aiola13.github.io/cosmos-servapps-unofficial/servapps/${file}/artefacts/${artefact}`)
-    }
-  }
+	if (fs.existsSync(`./servapps/${file}/artefacts`)) {
+		const artefacts = fs.readdirSync(`./servapps/${file}/artefacts`);
+		for (const artefact of artefacts) {
+			servapp.artefacts[
+				artefact
+			] = `https://aiola13.github.io/cosmos-servapps-unofficial/servapps/${file}/artefacts/${artefact}`;
+		}
+	}
 
-  servapp.icon = `https://aiola13.github.io/cosmos-servapps-unofficial/servapps/${file}/icon.png`
-  //Common Format,used by most
-  const YMLComposeSource =  `https://aiola13.github.io/cosmos-servapps-unofficial/servapps/${file}/docker-compose.yml`;
-  if(fs.existsSync(`./servapps/${file}/docker-compose.yml`)) {
-    servapp.compose = YMLComposeSource;
-  }
-  //Cosmos Legacy Format
-  const CosmosComposeSource =  `https://aiola13.github.io/cosmos-servapps-unofficial/servapps/${file}/cosmos-compose.json`; 
-  if(fs.existsSync(`./servapps/${file}/cosmos-compose.json`)) {
-    servapp.compose = CosmosComposeSource;
-    }
+	servapp.icon = `https://aiola13.github.io/cosmos-servapps-unofficial/servapps/${file}/icon.png`;
+	//Common Format,used by most
+	const YMLComposeSource = `https://aiola13.github.io/cosmos-servapps-unofficial/servapps/${file}/docker-compose.yml`;
+	if (fs.existsSync(`./servapps/${file}/docker-compose.yml`)) {
+		servapp.compose = YMLComposeSource;
+	}
+	//Cosmos Legacy Format
+	const CosmosComposeSource = `https://aiola13.github.io/cosmos-servapps-unofficial/servapps/${file}/cosmos-compose.json`;
+	if (fs.existsSync(`./servapps/${file}/cosmos-compose.json`)) {
+		servapp.compose = CosmosComposeSource;
+	}
 
-  servappsJSON.push(servapp)
+	servappsJSON.push(servapp);
 }
 
 // add showcase
-const _sc = ["Jellyfin", "Home Assistant", "Nextcloud"];
-const showcases = servappsJSON.filter((app) => _sc.includes(app.name));
+const _sc = ['Jellyfin', 'Home Assistant', 'Nextcloud'];
+const showcases = servappsJSON.filter(app => _sc.includes(app.name));
 
 let apps = {
-  "source": configFile.url,
-  "showcase": showcases,
-  "all": servappsJSON
-}
+	source: configFile.url,
+	showcase: showcases,
+	all: servappsJSON,
+};
 
-fs.writeFileSync('./servapps.json', JSON.stringify(servappsJSON, null, 2))
-fs.writeFileSync('./index.json', JSON.stringify(apps, null, 2))
+fs.writeFileSync('./servapps.json', JSON.stringify(servappsJSON, null, 2));
+fs.writeFileSync('./index.json', JSON.stringify(apps, null, 2));
 
 for (const servapp of servappsJSON) {
-  servapp.compose = `http://localhost:3000/servapps/${servapp.id}/cosmos-compose.json`
-  servapp.icon = `http://localhost:3000/servapps/${servapp.id}/icon.png`
-  for (let i = 0; i < servapp.screenshots.length; i++) {
-    servapp.screenshots[i] = servapp.screenshots[i].replace('https://aiola13.github.io/cosmos-servapps-unofficial', 'http://localhost:3000')
-  }
-  for (const artefact in servapp.artefacts) {
-    servapp.artefacts[artefact] = servapp.artefacts[artefact].replace('https://aiola13.github.io/cosmos-servapps-unofficial', 'http://localhost:3000')
-  }
+	servapp.compose = `http://localhost:3000/servapps/${servapp.id}/cosmos-compose.json`;
+	servapp.icon = `http://localhost:3000/servapps/${servapp.id}/icon.png`;
+	for (let i = 0; i < servapp.screenshots.length; i++) {
+		servapp.screenshots[i] = servapp.screenshots[i].replace(
+			'https://aiola13.github.io/cosmos-servapps-unofficial',
+			'http://localhost:3000',
+		);
+	}
+	for (const artefact in servapp.artefacts) {
+		servapp.artefacts[artefact] = servapp.artefacts[artefact].replace(
+			'https://aiola13.github.io/cosmos-servapps-unofficial',
+			'http://localhost:3000',
+		);
+	}
 }
 
-fs.writeFileSync('./servapps_test.json', JSON.stringify(apps, null, 2))
+fs.writeFileSync('./servapps_test.json', JSON.stringify(apps, null, 2));
